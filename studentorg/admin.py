a0d35521db3db09bda8737a78aa171b34a17e622
obeddebelen <obeddebelen@gmail.com>
2026-02-13 21:39:17 +0800

    models and admin working properly

--- studentorg/admin.py
+++ studentorg/admin.py
@@ -1,36 +1,36 @@
 from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
-# Task A - CollegeAdmin [cite: 816]
+
 @admin.register(College)
 class CollegeAdmin(admin.ModelAdmin):
-    list_display = ("college_name", "created_at", "updated_at") # [cite: 819]
-    search_fields = ("college_name",) # [cite: 821]
-    list_filter = ("created_at",) # [cite: 822]
+    list_display = ("college_name", "created_at", "updated_at") 
+    search_fields = ("college_name",) 
+    list_filter = ("created_at",) 
 
-# Task B - ProgramAdmin [cite: 825]
+
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
-    list_display = ("prog_name", "college") # [cite: 827]
-    search_fields = ("prog_name", "college__college_name") # [cite: 829]
-    list_filter = ("college",) # [cite: 831]
+    list_display = ("prog_name", "college") 
+    search_fields = ("prog_name", "college__college_name") 
+    list_filter = ("college",) 
 
-# Task C - OrganizationAdmin [cite: 834]
+
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ("name", "college", "description") # [cite: 837]
-    search_fields = ("name", "description") # [cite: 839]
-    list_filter = ("college",) # [cite: 841]
+    list_display = ("name", "college", "description") 
+    search_fields = ("name", "description") 
+    list_filter = ("college",) 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("student_id", "lastname", "firstname", "middlename", "program") # [cite: 644-645]
-    search_fields = ("lastname", "firstname") # [cite: 646]
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program") 
+    search_fields = ("lastname", "firstname") 
 
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined") # [cite: 649]
-    search_fields = ("student__lastname", "student__firstname") # [cite: 650, 655]
+    list_display = ("student", "get_member_program", "organization", "date_joined") 
+    search_fields = ("student__lastname", "student__firstname") 
 
     def get_member_program(self, obj):
         try:
